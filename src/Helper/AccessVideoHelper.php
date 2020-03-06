80668c5e80b9a072f2dab2ff19c7a261ffb2164b
Manol Trendafilov <m.trendafilov@propy.com>
2020-03-06 15:23:05 +0200

    restrict user to watch videos 10 times and after a certain period of time

---
+++ src/Helper/AccessVideoHelper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Helper;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AccessVideoHelper
+{
+    static User $user;
+    static function hasAccess(UserInterface $user, $configViewsNum = 10, $configWaitTimeInSeconds = 3600) {
+
+        $date = clone $user->getLastViewAt() ?? new \DateTime();
+
+        $date->modify(sprintf('+%s seconds', $configWaitTimeInSeconds));
+        $now = new \DateTime();
+
+        return (
+            $user->getIsAdmin() ||
+            ($user->getViews() > $configViewsNum && $now > $date) ||
+            ($user->getViews() < $configViewsNum)
+        );
+    }
+}
